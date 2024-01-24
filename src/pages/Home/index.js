219e9d9aa9a4d42739252a2219b4ef21ec7694f7
elishadiah <elishadiah326@gmail.com>
2024-01-25 01:43:27 +0700

    header section

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>This si the home page</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Ranking/index.js
@@ -0,0 +1,9 @@
+const Ranking = () => {
+    return (
+        <div>
+            <h1>This is Ranking page</h1>
+        </div>
+    )
+}
+
+export default Ranking;
