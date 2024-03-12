19bd62f5345460e95a02b710829b0911553a1f3d
elishadiah <elishadiah326@gmail.com>
2024-03-12 17:43:32 +0700

    url change

--- src/utility/http-client.js
+++ src/utility/http-client.js
@@ -2,9 +2,8 @@ import axios from "axios";
 import authService from "../services/auth.service";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://whale-app-osu76.ondigitalocean.app",
-  // baseURL: "https://backend.dartsfightclub.de",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://backend.dartsfightclub.de",
 });
 
 instance.interceptors.request.use(
